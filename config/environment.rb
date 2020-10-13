1c510869f4a85e6076ff2d17a5c3b88cc57f999e
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-10-13 21:29:17 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -15,4 +15,4 @@ DB = ActiveRecord::Base.establish_connection({
 if ENV["ACTIVE_RECORD_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
-require'./app/show_controller.rb'
+# require'./app/show_controller.rb'
